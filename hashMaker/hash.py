c42c81bc0d7a407b5dc9e84de0f6ceead6c73b07
Hugo Sanctorum <huugoo60de@gmail.com>
2018-05-28 16:19:51 +0200

    Hash done -- interface graphique en cours

--- hashMaker/hash.py
+++ hashMaker/hash.py
@@ -2,34 +2,108 @@
 
 # -*- coding: utf-8 -*-
 
-from random import *
+import os
+import tkinter as tk 
 
-str = input("sentence to hash : ")
+content = ""
 
 def mapValue(val,x1,y1,x2,y2):
 	z = x1/x2
 	w = y1/y2
 	return val * z/w
 
+def reverseString(string):
+	return string[::-1]
 
-def keyGen(str):
+def writeHash(keyhash):
+	file = open("DB.txt", "a")
+	file.write(keyhash)
+	file.close()
+
+def readHash():
+	file = open("DB.txt", "r")
+	keyhash = file.read()
+	file.close()
+	return keyhash
+
+def delHash():
+	if (os.path.isfile("./DB.txt")):
+		os.remove("./DB.txt")
+	open("./DB.txt", 'a')
+
+def isHashSet():
+	if(readHash() == ""):
+		return False
+	else:
+		return True
+
+def keyGen(string):
 	key=""
-	for i in range(256):
-		char = randint(1,9)
-		moy=0
-		for k in str:
-			moy = moy + ord(k)
-		moy = moy / len(str)
-		char = int(mapValue(char,1,9,21,moy))
-		key = key + chr(char)
-	return key	
+	val=ord(string[0])^ord(string[-1]) 
+	while len(key)<256:
+		for k in string:
+			char = ord(k)^val
+			val = mapValue(ord(k),1,126,1,9)
+			val = int(val * char / 2)
+			key = key + str(val)
+	if(len(key)>256):
+		key = key[:256]		
+	return key
 
+def hashProcess(string):
+	key = int(keyGen(string))
+	reverseKey = int(keyGen(reverseString(string)))
+	rotg = key<<len(string)
+	rotd = reverseKey>>len(string)
+	merge = str(rotg)+str(rotd)
+	if(len(merge)>256):
+		merge = merge[:256]
+	return merge
 
-def algo(str,clé):
-	pass
+def compare(string, keyhash):
+	toComp = hash(string)
+	if(toComp == keyhash):
+		return True
+	else:
+		return False	
 
-def hash(str,algo):
-	pass
+def showDB():
+	content = readHash()
+	labelDB.configure(text='content')		
 
+def process():
+	if isHashSet():
+		keyhash = readHash()
+		if compare(inText.get(), keyhash):
+			labelStatus.configure(text='Password is good', fg="green")
+		else:
+			labelStatus.configure(text='Password is incorrect !', fg="red")
+	else:
+		keyhash = inText.get()
+		if not keyhash == "":
+			writeHash(keyhash)
+			labelStatus.configure(text='Password has been set !', fg="green")
+		else:
+			labelStatus.configure(text='Text is empty, please try to re-enter you password', fg="red")
 
-print(keyGen(str))
+window = tk.Tk()
+window.title("Hash Maison")
+window.geometry("300x500")
+
+labelInfo = tk.Label(window, text="Please enter your password below").pack()
+
+inText = tk.Entry(window, show="*").pack()
+
+buttonValid = tk.Button(window, text="Validate", command=process).pack()
+
+labelStatus = tk.Label(window, text="Hash Not Set ! Please start typing your password", fg="red").pack()
+
+buttonDB = tk.Button(window, text="Show DB", command=showDB).pack()
+
+buttonDel = tk.Button(window, text="Delete Hash", command=delHash).pack()
+
+buttonQuit = tk.Button(window, text="Quitter l'application", command=window.quit).pack()
+
+labelDB = tk.Label(window, text=content).pack()
+
+window.mainloop()
